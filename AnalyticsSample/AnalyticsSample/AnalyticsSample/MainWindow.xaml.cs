2fa3c257f574d1ec336de3fb8bb857ba355ae097
Shreya Bastikar <shreyabastikar@gmail.com>
2015-04-14 20:10:23 -0400

    AnalyticsLog and AnalyticsSample first commit

---
+++ AnalyticsSample/AnalyticsSample/AnalyticsSample/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using mcMath;
+namespace AnalyticsSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void baseWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Escape:
+                    Application.Current.Shutdown();
+                    break;
+            }
+        }
+
+        int leftSessionID=0;
+        bool left = false;
+        bool right = false;
+        private void butLeft_Click(object sender, RoutedEventArgs e)
+        {
+            grdLeft.Visibility = Visibility.Visible;
+            AnalyticsLog clsobj = new AnalyticsLog();
+            long lRes = clsobj.Add(23, 40);
+            clsobj.Extra = false;
+
+            Console.WriteLine(lRes.ToString());
+            left = true;
+            LogRecord obj = clsobj.PostSessionStart(1, butLeft.Name, "table");
+            int ret = clsobj.PostSessionEnd(obj);
+      
+            // Register a selection of this item and save a session token 
+            // so that we can track duration that the left panel is open.
+            // Pseudo code:
+
+            //leftSessionID = AnalyticsModule.PostSessionStart(butLeft.Name);
+
+            // In this pseudo code, two things happen:
+            // 1) The selection of an object is recorded in the analytics database.
+            // 2) Also recorded at that moment is a begin time. 
+            //A unique identifier is passed back so that when the panel closes, 
+            // the analytics module can record the end time so that we get a duration.
+
+
+            // Since we may not always want to get the duration, we could break this into two calls to the analytics module 
+            // so that either of them may be optional:
+
+            //AnalyticsModule.PostSelection("object", 419);
+            //leftSessionID = AnalyticsModule.StartSession("object", 419);
+
+            // The parameters "object" and 419 would be information that comes from a database.
+        }
+
+        private void butRight_Click(object sender, RoutedEventArgs e)
+        {
+            grdRight.Visibility = Visibility.Visible;
+            right = true;
+            // Pseudo code:
+            //AnalyticsModule.PostSessionEnd(leftSessionID);
+        }
+
+        int rightSessionID;
+        private void butLeftClose_Click(object sender, RoutedEventArgs e)
+        {
+            if (left)
+            {
+                left = false;
+
+
+            }
+            
+            grdLeft.Visibility = Visibility.Hidden;
+            Console.WriteLine("***************** {0}",e.Source);
+            // Pseudo code:
+            //rightSessionID = AnalyticsModule.PostSessionStart(butRight.Name);
+        }
+
+        private void butRightClose_Click(object sender, RoutedEventArgs e)
+        {
+            right = false;
+            grdRight.Visibility = Visibility.Hidden;
+
+            // Pseudo code:
+            //AnalyticsModule.PostSessionEnd(rightSessionID);
+        }
+    }
+}
---
+++ mcMath/mcMath/LogRecord.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LINQtoCSV;
+
+namespace mcMath
+{
+    public class LogRecord
+    {
+        public LogRecord() { }
+        public LogRecord(int ID, string Name, string Table) {
+
+            this.ID = ID;
+            this.Name = Name;
+            this.Table = Table;
+        
+        }
+
+        [CsvColumn(Name = "ID", FieldIndex = 1)]
+        public int ID { get; set; }
+
+        [CsvColumn(Name = "Name", FieldIndex = 2)]
+        public string Name { get; set; }
+
+        [CsvColumn(Name = "Table", FieldIndex = 3)]
+        public string Table { get; set; }
+
+        [CsvColumn(Name = "BeginTime", FieldIndex = 4, OutputFormat = "dd MMM HH:mm:ss")]
+        public DateTime BeginTime { get; set; }
+
+        [CsvColumn(Name = "EndTime", FieldIndex = 5, OutputFormat = "dd MMM HH:mm:ss")]
+        public DateTime EndTime { get; set; }
+
+
+    }
+
+    public class AnalyticsLog
+    {
+        private bool bTest = false;
+        public AnalyticsLog()
+        {
+            // 
+            // TODO: Add constructor logic here 
+            // 
+        }
+        /// <summary> 
+        /// //This is a test method 
+        /// </summary> 
+        public void mcTestMethod()
+        {
+        }
+        /// <summary>
+        /// //This is a test property
+        /// </summary>
+        public bool Extra
+        {
+            get
+            {
+                return bTest;
+            }
+            set
+            {
+                bTest = Extra;
+            }
+        }
+
+        public long Add(long val1, long val2)
+        {
+            return val1 + val2;
+        }
+        public LogRecord PostSessionStart(int ID, string Name, string Table)
+        {
+            
+
+            return new LogRecord(ID, Name, Table);
+        }
+
+        public int PostSessionEnd(LogRecord sessionobj)
+        {
+
+            CsvContext cc = new CsvContext();
+
+           
+            int leftsessionid = 1;
+           
+            CsvFileDescription outputFileDescription = new CsvFileDescription
+            {
+                QuoteAllFields = false,
+                SeparatorChar = ',', // tab delimited
+                FirstLineHasColumnNames = true,
+
+            };
+
+
+            List<LogRecord> LogRecord = new List<LogRecord>();
+            LogRecord.Add(new LogRecord { ID = leftsessionid, Name = sessionobj.Name });
+            
+
+            cc.Write(
+                LogRecord,
+                "logrecord.csv",
+                outputFileDescription);
+
+            return 1;
+        }
+    }
+}
